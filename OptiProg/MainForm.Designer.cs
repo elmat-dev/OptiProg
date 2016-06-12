e68191b187e5ec09eb68087317086fd6c3374be2
Elmat repository <g.chmiel@fibrain.pl>
2016-06-13 00:32:21 +0200

    First upload
    Thats the first test upload

---
+++ OptiProg/MainForm.Designer.cs
@@ -0,0 +1,340 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: g.chmiel
+ * Date: 2016-06-10
+ * Time: 10:57
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace OptiProg
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Index;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Hex;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Dec;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Ascii;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+		private System.Windows.Forms.DataGridViewComboBoxColumn Value;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button button1;
+
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button5 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Hex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Dec = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Ascii = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Value = new System.Windows.Forms.DataGridViewComboBoxColumn();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.button1 = new System.Windows.Forms.Button();
+			this.statusStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.toolStripStatusLabel1,
+			this.toolStripStatusLabel2});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 567);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(1113, 22);
+			this.statusStrip1.TabIndex = 2;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(127, 17);
+			this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+			// 
+			// toolStripStatusLabel2
+			// 
+			this.toolStripStatusLabel2.AutoSize = false;
+			this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+			this.toolStripStatusLabel2.Size = new System.Drawing.Size(220, 17);
+			this.toolStripStatusLabel2.Text = "test";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(515, 46);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "LED On";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(515, 76);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "LED Off";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(515, 106);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(13, 29);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(130, 40);
+			this.textBox1.TabIndex = 6;
+			this.textBox1.Text = "0";
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(261, 96);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 11;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+			"10G Optical ER",
+			"10G Optical LRM",
+			"10G Optical LR",
+			"10G Optical SR",
+			"1G Optical SX",
+			"1G Optical-LX",
+			"1G Copper Passsive",
+			"1G Copper Active"});
+			this.comboBox1.Location = new System.Drawing.Point(792, 146);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 12;
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.AllowUserToAddRows = false;
+			this.dataGridView2.AllowUserToDeleteRows = false;
+			this.dataGridView2.AllowUserToResizeColumns = false;
+			this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.Control;
+			this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.dataGridView2.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+			dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+			dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+			dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+			dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			this.dataGridView2.ColumnHeadersHeight = 21;
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+			this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+			this.Index,
+			this.Address,
+			this.Hex,
+			this.Dec,
+			this.Ascii,
+			this.Description,
+			this.Value});
+			dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+			dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Window;
+			dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.ControlText;
+			dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+			this.dataGridView2.DefaultCellStyle = dataGridViewCellStyle9;
+			this.dataGridView2.Location = new System.Drawing.Point(13, 146);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+			dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+			dataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.Control;
+			dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.WindowText;
+			dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.dataGridView2.RowHeadersDefaultCellStyle = dataGridViewCellStyle10;
+			this.dataGridView2.RowHeadersVisible = false;
+			this.dataGridView2.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+			this.dataGridView2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+			this.dataGridView2.ShowCellErrors = false;
+			this.dataGridView2.ShowEditingIcon = false;
+			this.dataGridView2.Size = new System.Drawing.Size(592, 216);
+			this.dataGridView2.TabIndex = 13;
+			this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView2CellContentClick);
+			// 
+			// Index
+			// 
+			dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+			this.Index.DefaultCellStyle = dataGridViewCellStyle2;
+			this.Index.HeaderText = "Index";
+			this.Index.Name = "Index";
+			this.Index.Width = 40;
+			// 
+			// Address
+			// 
+			dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+			dataGridViewCellStyle3.Format = "X2";
+			dataGridViewCellStyle3.NullValue = "00";
+			this.Address.DefaultCellStyle = dataGridViewCellStyle3;
+			this.Address.HeaderText = "Addr";
+			this.Address.Name = "Address";
+			this.Address.Width = 40;
+			// 
+			// Hex
+			// 
+			dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+			dataGridViewCellStyle4.Format = "X2";
+			dataGridViewCellStyle4.NullValue = "00";
+			this.Hex.DefaultCellStyle = dataGridViewCellStyle4;
+			this.Hex.HeaderText = "Hex";
+			this.Hex.Name = "Hex";
+			this.Hex.Width = 30;
+			// 
+			// Dec
+			// 
+			dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+			this.Dec.DefaultCellStyle = dataGridViewCellStyle5;
+			this.Dec.HeaderText = "Dec";
+			this.Dec.Name = "Dec";
+			this.Dec.Width = 30;
+			// 
+			// Ascii
+			// 
+			dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+			this.Ascii.DefaultCellStyle = dataGridViewCellStyle6;
+			this.Ascii.HeaderText = "Ascii";
+			this.Ascii.Name = "Ascii";
+			this.Ascii.Width = 30;
+			// 
+			// Description
+			// 
+			dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			this.Description.DefaultCellStyle = dataGridViewCellStyle7;
+			this.Description.HeaderText = "Description";
+			this.Description.Name = "Description";
+			this.Description.Width = 200;
+			// 
+			// Value
+			// 
+			dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			this.Value.DefaultCellStyle = dataGridViewCellStyle8;
+			this.Value.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.ComboBox;
+			this.Value.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.Value.HeaderText = "Value";
+			this.Value.Name = "Value";
+			this.Value.Width = 200;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(30, 378);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 14;
+			this.button1.Text = "Open file";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1113, 589);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dataGridView2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.statusStrip1);
+			this.Name = "MainForm";
+			this.Text = "OptiProg";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+		}
+}
---
+++ OptiProg/MainForm.cs
@@ -0,0 +1,226 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: g.chmiel
+ * Date: 2016-06-10
+ * Time: 10:57
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Timers;
+
+using HidSharp;
+
+namespace OptiProg
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		private static System.Timers.Timer aTimer;
+		HidDeviceLoader loader = new HidDeviceLoader();
+		public byte [] send = new byte[64];
+		public byte [] receive = new byte[64];
+		public HidStream stream;
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			aTimer = new System.Timers.Timer(200);
+			aTimer.Elapsed += OnTimedEvent;
+			aTimer.AutoReset = true;
+			aTimer.Enabled = true;
+
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+
+						
+			
+		/*
+		  			for (int i = 0; i < dataGridView2.Rows.Count; ++i)
+     		{
+         		DataGridViewComboBoxCell cell = dataGridView2.Rows[i].Cells[6] as DataGridViewComboBoxCell;
+       
+		             cell.Items.Clear();
+		             cell.Items.Add("one");
+		             cell.Items.Add("two");
+
+		     }
+		*/
+		dataGridView2.RowTemplate.Height = 18;
+
+		for (int i=0;i<128;i++)
+			dataGridView2.Rows.Add();
+		for (int i=0;i<128;i++) {
+			dataGridView2.Rows[i].Cells[0].Value = i;
+			dataGridView2.Rows[i].Cells[1].Value = i;
+		}
+		for (int i=20;i<36;i++) {
+			dataGridView2.Rows[i].Cells[0].Style.BackColor = Color.LightGreen;
+			dataGridView2.Rows[i].Cells[1].Style.BackColor = Color.LightGreen;
+			dataGridView2.Rows[i].Cells[2].Style.BackColor = Color.LightGreen;
+			dataGridView2.Rows[i].Cells[3].Style.BackColor = Color.LightGreen;
+			dataGridView2.Rows[i].Cells[4].Style.BackColor = Color.LightGreen;
+			dataGridView2.Rows[i].Cells[5].Style.BackColor = Color.LightGreen;
+			dataGridView2.Rows[i].Cells[6].Style.BackColor = Color.LightGreen;
+		}
+		for (int i=40;i<56;i++) {
+			dataGridView2.Rows[i].Cells[0].Style.BackColor = Color.SkyBlue;
+			dataGridView2.Rows[i].Cells[1].Style.BackColor = Color.SkyBlue;
+			dataGridView2.Rows[i].Cells[2].Style.BackColor = Color.SkyBlue;
+			dataGridView2.Rows[i].Cells[3].Style.BackColor = Color.SkyBlue;
+			dataGridView2.Rows[i].Cells[4].Style.BackColor = Color.SkyBlue;
+			dataGridView2.Rows[i].Cells[5].Style.BackColor = Color.SkyBlue;
+			dataGridView2.Rows[i].Cells[6].Style.BackColor = Color.SkyBlue;
+		}
+		for (int i=68;i<84;i++) {
+			dataGridView2.Rows[i].Cells[0].Style.BackColor = Color.LightCoral;
+			dataGridView2.Rows[i].Cells[1].Style.BackColor = Color.LightCoral;
+			dataGridView2.Rows[i].Cells[2].Style.BackColor = Color.LightCoral;
+			dataGridView2.Rows[i].Cells[3].Style.BackColor = Color.LightCoral;
+			dataGridView2.Rows[i].Cells[4].Style.BackColor = Color.LightCoral;
+			dataGridView2.Rows[i].Cells[5].Style.BackColor = Color.LightCoral;
+			dataGridView2.Rows[i].Cells[6].Style.BackColor = Color.LightCoral;
+		}
+		
+	}
+
+		
+	
+		
+		
+		
+		
+		
+
+
+		void OnTimedEvent(Object source, ElapsedEventArgs e)												// it's and event which is checking for device presence in the system, fired by timer set in main application currently
+		{
+			var device = loader.GetDevices(1155, 22352).FirstOrDefault();								// check if device is present
+			if (device == null) {																		// if it is not present in the system then:
+				toolStripStatusLabel1.ForeColor = Color.Red;											// change status bar label font color to RED and
+				toolStripStatusLabel1.Text = "Device not detected";										// set status bar label text accordingly
+			}
+			
+			else {																						// if it is present in the system then:
+				toolStripStatusLabel1.ForeColor = Color.Green;											// change status bar label font color to GREEN and
+				toolStripStatusLabel1.Text = "Device detected";											// set status bar label text accordingly
+			}
+			
+
+			device.TryOpen (out stream);
+			stream.Read(receive);
+
+			if ( receive[2] == 1 ){
+				toolStripStatusLabel2.Text = "Button pressed";	
+				Thread.Sleep(100);
+			}
+			receive[2] = 0;
+				toolStripStatusLabel2.Text = " ";
+
+			this.statusStrip1.Update();																		// update status bar label text to show changes
+
+		}
+		
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			var device = loader.GetDevices(1155, 22352).FirstOrDefault();
+  			device.TryOpen(out stream);
+  			send[3] = 0x33;
+  			//send[4] = 0;
+  			stream.Write(send); 
+
+  			//toolStripStatusLabel2.Text = " Button 2";
+			//statusStrip1.Refresh();				
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			var device = loader.GetDevices(1155, 22352).FirstOrDefault();
+  			device.TryOpen(out stream);
+  			send[3] = 0x34;
+  			//send[4] = 0;
+  			stream.Write(send); 
+  			//toolStripStatusLabel2.Text = " Button 3";
+			//statusStrip1.Refresh();		
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+			//var device = loader.GetDevices(1155, 22352).FirstOrDefault();
+  			//device.TryOpen (out stream);
+  			//stream.Read(receive);
+
+  			//if (receive[2]==1) MessageBox.Show("Button press detected!");
+   
+  		
+						var device = loader.GetDevices(1155, 22352).FirstOrDefault();
+  			device.TryOpen(out stream);
+			send[4]= byte.Parse(textBox1.Text);
+  			stream.Write(send); 
+  			
+
+
+  			
+		}
+
+		void Button5Click(object sender, EventArgs e)
+		{
+			
+
+		}
+		
+
+
+		void MainFormLoad(object sender, EventArgs e)
+		{
+	
+		}
+		void DataGridView2CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+	
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			openFileDialog1.Title = "Open File...";
+            openFileDialog1.Filter = "Binary File (*.bin)|*.bin";
+            openFileDialog1.InitialDirectory = @"C:\";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
+                var br = new BinaryReader(fs);
+
+				for (int i=0;i<128;i++) {
+					var temp = new byte();
+					temp = br.ReadByte();
+					dataGridView2.Rows[i].Cells[2].Value = temp;
+					dataGridView2.Rows[i].Cells[3].Value = temp;
+					dataGridView2.Rows[i].Cells[4].Value = Convert.ToChar(temp);
+				}
+                fs.Close();
+                br.Close();
+               
+            }
+		}
+		
+
+		
+		
+		
+		
+		
+		
+		
+	}
+}
